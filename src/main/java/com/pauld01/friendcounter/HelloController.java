abce7d9c946d79d72f574acffcf3fc8344d30005
Paul <paul.duplessi@hotmail.fr>
2022-01-13 15:26:59 +0100

    first commit HelloApplication

---
+++ src/main/java/com/pauld01/friendcounter/HelloController.java
@@ -0,0 +1,14 @@
+package com.pauld01.friendcounter;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.pauld01.friendcounter {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.pauld01.friendcounter to javafx.fxml;
+    exports com.pauld01.friendcounter;
+}
